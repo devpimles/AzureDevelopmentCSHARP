8156dea1e811f3e1cfef55dda5457a76007fd5c4
devpimles <devpimles@gmail.com>
2026-02-19 21:13:20 +0000

    ASP.NET Core Web API (.NET 8) using controller-based REST endpoints instead of Minimal APIs

---
+++ src/Api/Controllers/HealthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                status = "ok",
+                service = "api",
+                timestamp = DateTime.UtcNow
+            });
+        }
+    }
+}
